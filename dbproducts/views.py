8ba69cd39eac4e6f96833f2b7207e19d96ee79f6
Jc4Mn <guillaumecarcema@gmail.com>
2019-11-21 18:46:59 +0100

    Creating database for dbproduct app

--- dbproducts/views.py
+++ dbproducts/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def dbprod(request):
+    return HttpResponse("Will display something soon")
--- mydjango/settings.py
+++ mydjango/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'dbproducts',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -75,11 +77,14 @@ WSGI_APPLICATION = 'mydjango.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get("BDD"),
+        'USER': os.environ.get("USER"),
+        'PASSWORD': os.environ.get("PW_POSTGRES"),
+        'HOST': '',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
