e19a2cc61f279a0a80063897b1337a678ff2de23
Jc4Mn <guillaumecarcema@gmail.com>
2020-02-26 10:49:53 +0100

    bootstrap integration

--- mydjango/settings.py
+++ mydjango/settings.py
@@ -25,7 +25,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'g&04hna_b!rz$6=2cq@rqlwuco%^f00gr=38xne+sewx)6q%)-'
+SECRET_KEY = os.environ.get("KEY_PROD")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
