1fd5abc046d8e7130f5f525805d89bcd27306d7a
Jc4Mn <guillaumecarcema@gmail.com>
2020-04-03 16:19:55 +0200

    Fixed test for custom command

--- dbproducts/tests/test_command_dbproducts.py
+++ dbproducts/tests/test_command_dbproducts.py
@@ -1,15 +1,15 @@
 # Tests meant for commands
 
-from io import StringIO
 from unittest.mock import patch
 from django.test import TestCase
 from django.core.management import call_command
+from dbproducts.models import Product
 
 class CommandTestCase(TestCase):
     @patch("requests.get")
     def test_populate_db_works_correctly(self, mock_get):
         mock_get.return_value.json.return_value = {
-            "products":{
+            "products":[{
                 "_id":123456,
                 "product_name_fr":"jambon",
                 "image_front_url":"img.com",
@@ -18,6 +18,10 @@ class CommandTestCase(TestCase):
                 "url":"anotherlink.com",
                 "categories_hierarchy":["viande"]
             },
+            ]
         }
-    out = StringIO()
-    call_command("populate_db", stdout=out)
+        call_command("populate_db")
+        product = Product.objects.all()
+        self.assertEqual(len(product), 1)
+
+        self.assertEqual(product[0].product_name, "jambon")
